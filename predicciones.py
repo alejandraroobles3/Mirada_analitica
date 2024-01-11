b84650e9192c62a13eeb623054e4b351353539a3
alejandraroobles3 <64538989+alejandraroobles3@users.noreply.github.com>
2024-01-11 13:14:15 -0600

    limpieza de los datos

--- predicciones.py
+++ predicciones.py
@@ -1,31 +1,31 @@
 import joblib
 import spacy
 import psycopg2
+import codecs
 
 nlp= spacy.load('es_core_news_sm')
 
 clf=joblib.load('entrenamiento/modelo_entrenado.pkl')
+
+# Abre el archivo en modo lectura
+with codecs.open('txt/claudia.txt', 'r', 'UTF-8') as f:
+    # Itera sobre cada línea en el archivo
+    lines=[]
+    for line in f:
+        lines.append(line)
 
 def preprocess_text(text):
     doc = nlp(text)
     return ' '.join([token.lemma_ for token in doc if not token.is_punct and not token.is_stop])
 
 
-new_text = ""
-preprocessed_new_text = preprocess_text(new_text)
+for l in range(0,200):
+    new_text = lines[l]
+    preprocessed_new_text = preprocess_text(new_text)
+    
+    prediction= clf.predict([preprocessed_new_text])[0]
+    print(f'Clasificación del nuevo texto: {prediction}',lines[l])
 
-prediction= clf.predict([preprocessed_new_text])[0]
-print(f'Clasificación del nuevo texto: {prediction}')
 
 
-def agregar_dato(d,p,rs,t,e):
-    con=psycopg2.connect(database="mirada_analitica", user="postgres",
-    password="Ale123roblesmora",host="localhost")
-    cursor1=con.cursor()
 
-    dato=[d,p,rs,t,e]
-
-    cursor1.execute("insert into analisis.dato (dato,id_prediccion,id_red_social,id_tema,id_eleccion) values (%s,%s,%s,%s,%s) ",dato)
-    con.commit()
-    cursor1.close()
-    con.close()
--- python/inprovisado.py
+++ python/inprovisado.py
@@ -1,104 +1,23 @@
+import codecs
 
-import pandas as pd
-import numpy as np
-
-data=pd.read_csv('entrenamiento/IMDB Dataset SPANISH.csv')
-
-x=np.asanyarray(data.drop(columns=['Index','review_en','sentiment']))
-print(x)
-
-"""from bs4 import BeautifulSoup
-import requests
-import sys
-import json
+# Abre el archivo en modo lectura
+with codecs.open('txt/claudia.txt', 'r', 'UTF-8') as f:
+    # Itera sobre cada línea en el archivo
+    lines=[]
+    for line in f:
+        lines.append(line)
+        
+print(lines[0])
 
 
-def usage():
-    msg = 
-    Please use the below command to use the script.
-    python script_name.py twitter_username
-    
-    print(msg)
-    sys.exit(1)
+def agregar_dato(d,p,rs,t,e):
+    con=psycopg2.connect(database="mirada_analitica", user="postgres",
+    password="Ale123roblesmora",host="localhost")
+    cursor1=con.cursor()
 
-def get_tweet_text(tweet):
-    tweet_text_box = tweet.find("p", {"class": "TweetTextSize TweetTextSize--normal js-tweet-text tweet-text"})
-    if tweet_text_box:
-        images_in_tweet_tag = tweet_text_box.find_all("a", {"class": "twitter-timeline-link u-hidden"})
-        tweet_text = tweet_text_box.text
-        for image_in_tweet_tag in images_in_tweet_tag:
-            tweet_text = tweet_text.replace(image_in_tweet_tag.text, '')
-        return tweet_text.strip()
-    return None
+    dato=[d,p,rs,t,e]
 
-def get_this_page_tweets(soup):
-    tweets_list = list()
-    tweets = soup.find_all("li", {"data-item-type": "tweet"})
-    for tweet in tweets:
-        tweet_data = get_tweet_text(tweet)
-        if tweet_data:
-            tweets_list.append(tweet_data)
-            print(".", end="")
-            sys.stdout.flush()
-    return tweets_list
-
-import twint
-
-def get_tweets_data(username):
-    c = twint.Config()
-    c.Username = username
-    c.Store_object = True
-    c.Limit = 10  # Set the limit to the number of tweets you want
-    twint.run.Search(c)
-    tweets_list = [tweet.tweet for tweet in twint.output.tweets]
-    return tweets_list
-
-
-def dump_data(username, tweets):
-    filename = username + "_twitter.json"
-    print("\nDumping data in file " + filename)
-    data = dict()
-    data["tweets"] = tweets
-    with open(filename, 'w') as fh:
-        json.dump(data, fh)
-    return filename
-
-def get_username():
-    # if username is not passed
-    if len(sys.argv) < 2:
-        usage()
-    username = sys.argv[1].strip().lower()
-    if not username:
-        usage()
-    return username
-
-def start(username=None):
-    username = get_username()
-    url = "http://www.twitter.com/" + username
-    print("\n\nDownloading tweets for " + username)
-    response = None
-    try:
-        response = requests.get(url)
-    except Exception as e:
-        print(repr(e))
-        sys.exit(1)
-
-    if response.status_code != 200:
-        print("Non-success status code returned " + str(response.status_code))
-        sys.exit(1)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    if soup.find("div", {"class": "errorpage-topbar"}):
-        print("\n\n Error: Invalid username.")
-        sys.exit(1)
-
-    tweets = get_tweets_data(username, soup)
-    # dump data in a text file
-    dump_data(username, tweets)
-    print(str(len(tweets)) + " tweets dumped.")
-
-start()
-
-
-"""
+    cursor1.execute("insert into analisis.dato (dato,id_prediccion,id_red_social,id_tema,id_eleccion) values (%s,%s,%s,%s,%s) ",dato)
+    con.commit()
+    cursor1.close()
+    con.close()
