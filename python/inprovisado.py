d6bb1f928a830a4aecd9516b308d28a0ac3c34b5
alejandraroobles3 <64538989+alejandraroobles3@users.noreply.github.com>
2024-02-09 11:20:22 -0600

    texto procesado y etiquetado

--- python/inprovisado.py
+++ python/inprovisado.py
@@ -2,23 +2,29 @@ import psycopg2
 import pandas as pd
 
 data=pd.read_csv('csv/xochitl.csv')
+def pred(prediccion):
+    
+    id=[]
+    for i in prediccion:
+        if i == 'positivo':
+            id.append(1)
+        else: 
+            id.append(2)
+    
+    return id
 
 
-
-
-def agregar_dato(d,p,rs,t,e):
+def agregar_dato(dat,pred,estado,rs,tem,cand):
     con=psycopg2.connect(database="mirada.analitica", user="postgres",
     password="Ale123roblesmora",host="localhost")
     cursor1=con.cursor()
 
-    dato=[d,p,rs,t,e]
+    dato=[dat,pred,estado,rs,tem,cand]
 
-    cursor1.execute("insert into analisis.dato (dato,id_prediccion,id_estado,id_red_social,id_tema,id_candidato) values (%s,%s,%s,%s,%s) ",dato)
+    cursor1.execute("insert into dato (dato,id_prediccion,id_estado,id_red_social,id_tema,id_candidato) values (%s,%s,%s,%s,%s,%s) ",dato)
     con.commit()
     cursor1.close()
     con.close()
-
-print(data['dato'][1])
 
 dato=[]
 prediccion=[]
@@ -26,6 +32,19 @@ prediccion=[]
 dato=data['dato']
 prediccion=data['predicciones']
 red_social=1
+candidato=2
+estado=33
+tema=1
+id=[]
+
+id=pred(prediccion)
+
+print(len(id), len(dato))
+
+for i in range(0,len(dato)):
+    d=dato[i]
+    p=id[i]
+    agregar_dato(d,p,estado,red_social,tema,candidato)
 
 
-print(prediccion)
+
