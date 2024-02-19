c88df4e38a0a174bd762b07f33a978360e53d80f
alejandraroobles3 <64538989+alejandraroobles3@users.noreply.github.com>
2024-02-19 13:20:29 -0600

    html y css modificado

--- python/inprovisado.py
+++ python/inprovisado.py
@@ -1,7 +1,10 @@
 import psycopg2
 import pandas as pd
 
-data=pd.read_csv('csv/jorge.csv')
+#8627 sin datos de xohictl
+#con 14303
+
+data=pd.read_csv('csv/xochitl.csv')
 def pred(prediccion):
     
     id=[]
@@ -15,7 +18,7 @@ def pred(prediccion):
 
 
 def agregar_dato(dat,pred,estado,rs,tem,cand):
-    con=psycopg2.connect(database="mirada_analitica", user="postgres", password="Ale123roblesmora",host="localhost", port="5433")
+    con=psycopg2.connect(database="mirada_analitica", user="administrador", password="Ale123roblesmora",host="mirada.postgres.database.azure.com", port="5432")
     cursor1=con.cursor()
 
     dato=[dat,pred,estado,rs,tem,cand]
@@ -31,8 +34,8 @@ prediccion=[]
 dato=data['dato']
 prediccion=data['predicciones']
 red_social=1
-candidato=2
-estado=1
+candidato=1
+estado=33
 tema=1
 id=[]
 
@@ -40,7 +43,7 @@ id=pred(prediccion)
 
 print(len(id), len(dato))
 
-for i in range(0,len(dato)):
+for i in range(8666,len(dato)):
     d=dato[i]
     p=id[i]
     agregar_dato(d,p,estado,red_social,tema,candidato)
